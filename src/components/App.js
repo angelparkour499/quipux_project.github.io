c9be779e20df373cb0c5e71d00e4c2d685689e35
Angel <angel_epa_luses@hotmail.com>
2019-08-02 01:59:24 -0500

    0.2

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
         <Switch>
           <Route exact path={'/'} component={Home} />
           <Route exact path={'/article'} component={Article} />
-</Switch>
+          </Switch>
   </Layout>
     </BrowserRouter>
   );
